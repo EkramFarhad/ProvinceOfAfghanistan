a518b0c90204905154a5810ff3e9a17a582f0422
EkramFarhad <akram.farrhad@gmail.com>
2023-03-29 12:40:19 -0700

    Kabul is capital of Afghanistan

---
+++ src/Kabul.java
@@ -0,0 +1,5 @@
+public class Kabul {
+    String name;
+    String capital;
+    String population;
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Kabul kabul = new Kabul();
+        kabul.name = "KABUL";
+        kabul.capital= "Of Afghanistan";
+        kabul.population= 1234567;
+    }
+}
